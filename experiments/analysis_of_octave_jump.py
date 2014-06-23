db0deea89c69ddd2728babb51452a6198036e976
HKMogul <hilary.mogul@gmail.com>
2014-06-23 09:48:37 -0400

    analysis of key shift experiment now works

--- experiments/analysis_of_octave_jump.py
+++ experiments/analysis_of_octave_jump.py
@@ -14,14 +14,25 @@ other_shift_files = open('../analytic-files/octave_shift_process-other_shift.txt
 
 path_to_txt = '../data/cal500_txt/Clean_MIDIs-path_to_cal500_path.txt'
 path_file = open(path_to_txt, 'rb')
+amt_zero = 0
+amt_other = 0
 filereader = csv.reader(path_file, delimiter='\t')
 for row in filereader:
-  output_midi_filename = BASE_PATH+'/'+OUTPUT_PATH+'/'+row[0]
+  midi_filename = BASE_PATH+'/Clean_MIDIs/'+row[0]
+  output_midi_filename = midi_filename.replace('Clean_MIDIs', OUTPUT_PATH)
   diagnostic_filename = os.path.splitext(output_midi_filename)[0]+'-interval_diagnostics.txt'
-  diagnostic = open(diagnostic_filename,'r').read()
-  if 'Minimum Interval via dot: 0' in diagnostic:
-    zero_shift_files.write(output_midi_filename+'\n')
+  if os.path.exists(diagnostic_filename):
+    diagnostic = open(diagnostic_filename,'r').read()
+    if 'Minimum Interval via dot: 0' in diagnostic:
+      zero_shift_files.write(output_midi_filename+'\n')
+      amt_zero += 1
+    else:
+      other_shift_files.write(output_midi_filename+'\n')
+      amt_other += 1
   else:
-    other_shift_files.write(output_midi_filename+'\n')
+    print diagnostic_filename + " not found."
+print "Total processed: {} files".format(str(amt_zero + amt_other))
+print "Amount of zero shift files: {}".format(amt_zero)
+print "Amount of other files: {}".format(amt_other)
 zero_shift_files.close()
 other_shift_files.close()
