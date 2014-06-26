cc1c19b282d74d5e2da8c9f39ea7f6c59b5601ef
HKMogul <hilary.mogul@gmail.com>
2014-06-26 12:33:41 -0400

    removes extra function, renames variables to nonmisleading names

--- experiments/compare_folders.py
+++ experiments/compare_folders.py
@@ -7,60 +7,6 @@ import glob
 
 ''' Data processing to find differences in different folders of results'''
 
-def compare_paths(cqt_mat_path, piano_mat_path):
-  ''' Checks if alignment paths are the same, returns true if that is the case, false otherwise '''
-  nErrors = 0
-  total_possible = 1
-  cqt_mat = scipy.io.loadmat(cqt_mat_path)
-  p1 = cqt_mat['p']
-  p1 = p1[0][:]
-  q1 = cqt_mat['q']
-  q1 = q1[0][:]
-
-  piano_mat = scipy.io.loadmat(piano_mat_path)
-  p2 = piano_mat['p']
-  p2 = p2[0][:]
-
-  q2 = piano_mat['q']
-  q2 = q2[0][:]
-
-  # first easy check,if the shapes are different no need to iterate through
-  if p1.shape[0] != p2.shape[0] or q1.shape[0] != q2.shape[0]:
-    # print "Sizes of " + piano_mat_path + " do not match."
-    nErrors += 1
-  # # check p vectors
-  # for a,b in zip(p1, p2):
-  #   if a != b:
-  #     nErrors += 1
-  #   total_possible+=1
-  # # check q vectors
-  # for a,b in zip(q1, q2):
-  #   if a != b:
-  #     nErrors += 1
-  #   total_possible+=1
-
-  # turn vectors into p,q tuples
-  pq1 = np.rot90(np.vstack((p1,q1)),3)
-
-  pq2 = np.rot90(np.vstack((p2,q2)),3)
-  cqt_list= pq1.tolist()
-  piano_list = pq2.tolist()
-  for pair in cqt_list:
-    if pair not in piano_list:
-      nErrors += 1
-    total_possible += 1
-
-  # pq1_tuple = tuple(map(tuple, pq1))
-  # pq2_tuple = tuple(map(tuple,pq2))
-  # check if #2 has the tuples of #1
-  # for t in pq1_tuple:
-  #   if t not in pq2_tuple:
-  #     nErrors += 1
-  #   total_possible += 1
-  print nErrors
-  print total_possible
-  return float(nErrors)/total_possible
-
 def ext_to_mat(filename):
   ''' Converts any pathname to have .mat at end '''
   file_path = os.path.splitext(filename)[0]
@@ -81,46 +27,34 @@ def vs_filename_to_path(filename):
   file_base = filename[loc:].split('_',2)
   return ext_to_mat(file_base[0]+'/'+file_base[1])
 
-def compare_paths(cqt_mat_path, piano_mat_path):
+def compare_paths(mat_path_1, mat_path_2):
   ''' Checks if alignment paths are the same, returns true if that is the case, false otherwise '''
   nErrors = 0
-  total_possible = 1
-  cqt_mat = scipy.io.loadmat(cqt_mat_path)
-  p1 = cqt_mat['p']
+  total_possible = 1 # we know there will be a size amount
+  mat_1 = scipy.io.loadmat(mat_path_1)
+  p1 = mat_1['p']
   p1 = p1[0][:]
-  q1 = cqt_mat['q']
+  q1 = mat_1['q']
   q1 = q1[0][:]
 
-  piano_mat = scipy.io.loadmat(piano_mat_path)
-  p2 = piano_mat['p']
+  mat_2 = scipy.io.loadmat(mat_path_2)
+  p2 = mat_2['p']
   p2 = p2[0][:]
 
-  q2 = piano_mat['q']
+  q2 = mat_2['q']
   q2 = q2[0][:]
 
   # first easy check,if the shapes are different no need to iterate through
   if p1.shape[0] != p2.shape[0] or q1.shape[0] != q2.shape[0]:
-    # print "Sizes of " + piano_mat_path + " do not match."
     nErrors += 1
-  # # check p vectors
-  # for a,b in zip(p1, p2):
-  #   if a != b:
-  #     nErrors += 1
-  #   total_possible+=1
-  # # check q vectors
-  # for a,b in zip(q1, q2):
-  #   if a != b:
-  #     nErrors += 1
-  #   total_possible+=1
 
-  # turn vectors into p,q tuples
   pq1 = np.rot90(np.vstack((p1,q1)),3)
 
   pq2 = np.rot90(np.vstack((p2,q2)),3)
-  cqt_list= pq1.tolist()
-  piano_list = pq2.tolist()
-  for pair in cqt_list:
-    if pair not in piano_list:
+  mat_list_1= pq1.tolist()
+  mat_list_2 = pq2.tolist()
+  for pair in mat_list_1:
+    if pair not in mat_list_2:
       nErrors += 1
     total_possible += 1
 
@@ -172,36 +106,7 @@ else:
     else:
       amt_diff += 1
       diff_file.write(file+'\t'+str(error_rate)+'\n')
-  # other solution: set theory- make a set of whichever is shorter
-  # if len(glob_1) > len(glob_2):
-  #   temp_glob = glob_1.replace(path_1,path_2)
-  #   print temp_glob
 
-
-# with open(path_to_csv) as csv_file:
-#   filereader = csv.reader(csv_file)
-#   next(filereader, None)
-#   for row in filereader:
-#     # if this was a successful file, get the .mat file of that one and
-#
-#     mat_path = path_to_530+os.path.splitext(row[0])[0]+'.mat.mat'
-#     piano_mat_path = piano_base_path+vs_filename_to_path(row[0])
-#     nErrors = compare_paths(mat_path, piano_mat_path)
-#     print os.path.basename(piano_mat_path) +" "+ str(nErrors)
-#     if int(row[2]) == 1:
-#       print 'analysing successful file'
-#       if nErrors == 0:
-#         amt_match += 1
-#         print piano_mat_path + " matches CQT path, will write down"
-#         success_file.write(piano_mat_path + '\t 0 \n')
-#       else:
-#         diff_file.write(piano_mat_path+ '\t'+ str(nErrors)+'\n')
-#     else:
-#       if nErrors == 0:
-#         amt_match += 1
-#         success_fail.write(piano_mat_path + '\t 0 \n')
-#       else:
-#         diff_fail.write(piano_mat_path+ '\t'+str(nErrors)+'\n')
 success_file.close()
 diff_file.close()
 print "Total # of files that match paths: {}".format(amt_match)
