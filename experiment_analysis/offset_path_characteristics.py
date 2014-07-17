f515c693b1edb0b150be48e1fdbb508b57450f93
HKMogul <hilary.mogul@gmail.com>
2014-07-17 15:12:25 -0400

    update for new Pretty MIDI syntax

--- experiment_analysis/offset_path_characteristics.py
+++ experiment_analysis/offset_path_characteristics.py
@@ -11,9 +11,9 @@ import csv
     including the variance and regressions of the offset over time '''
 def linear_fit_offsets(new_midi= None, old_midi = None,midi_filename = None, original_midi_filename = None):
   if new_midi == None:
-    new_midi = PrettyMIDI(midi.read_midifile(midi_filename))
+    new_midi = PrettyMIDI(midi_filename)
   if old_midi == None:
-    old_midi = PrettyMIDI(midi.read_midifile(original_midi_filename))
+    old_midi = PrettyMIDI(original_midi_filename)
   note_ons = np.array([note.start for instrument in old_midi.instruments for note in instrument.events])
   aligned_note_ons = np.array([note.start for instrument in new_midi.instruments for note in instrument.events])
   # diff = np.subtract(aligned_note_ons,note_ons)
@@ -24,6 +24,19 @@ def linear_fit_offsets(new_midi= None, old_midi = None,midi_filename = None, ori
   slope, intercept, r_value, p_value, std_err = stats.linregress(note_ons[0:(last)],diff)
   return slope, intercept, r_value, std_err, diff
 
+def vs_filename_to_path(filename):
+  ''' Extracts filenames of original files May 30 alignment was based on from their filenames.
+     Example: '''
+  # hacky way- iterate thru string until subset 0 to 3 is '_vs_', then use _ as delimiter to generate path
+  # then concat with base path to
+  loc = 0
+  for i in xrange(0, len(filename)):
+    if filename[i:(i+4)] == '_vs_':
+      loc = i+4
+      break
+  # get list of artist and file
+  file_base = filename[loc:].split('_',2)
+  return ext_to_mat(file_base[0]+'/'+file_base[1])
 def simple_statistics(new_midi= None, old_midi = None,midi_filename = None, original_midi_filename = None, diff = None):
   if diff == None:
     if new_midi == None:
@@ -43,21 +56,19 @@ def simple_statistics(new_midi= None, old_midi = None,midi_filename = None, orig
   diff_dev = np.std(diff)
   return diff_max, diff_min, diff_mean, diff_dev
 
-BASE_PATH = '../data/sanity'
-OUTPUT_PATH = os.path.join(BASE_PATH,'midi-alignment-exp-force-piano')
-txt_path = os.path.join(BASE_PATH,'sanity_paths.txt')
-path_file = open(txt_path, 'rb')
-filereader = csv.reader(path_file, delimiter = '\t')
-for row in filereader:
+def run_check(row):
   basename = row[0]
   midi_filename = os.path.join(OUTPUT_PATH, basename)
   # get original midi filename
-  original_midi_filename = os.path.join(BASE_PATH, 'midi',basename)
-  new_midi = PrettyMIDI(midi.read_midifile(midi_filename))
+  original_midi_filename = os.path.join(BASE_PATH, 'Clean_MIDIs',basename)
+  try:
+    new_midi = PrettyMIDI(midi.read_midifile(midi_filename))
+  except:
+    print "ERROR LOADING {}".format(basename)
+    return
   old_midi = PrettyMIDI(midi.read_midifile(original_midi_filename))
   slope, intercept, r_value, std_err, diff = linear_fit_offsets(new_midi = new_midi, old_midi = old_midi)
   diff_max, diff_min, diff_mean, diff_dev = simple_statistics(diff = diff)
-
   print os.path.basename(midi_filename)+ " alignment statistics: "
   print "slope: {}".format(slope)
   print "intercept: {}".format(intercept)
@@ -69,4 +80,21 @@ for row in filereader:
   print "Unweighted mean of difference: {}".format(diff_mean)
   print "Standard dev of difference: {}".format(diff_dev)
   print "----------"
+
+BASE_PATH = '../data/cal500_txt'
+OUTPUT_PATH = os.path.join(BASE_PATH,'midi-aligned-additive-dpmod-piano')
+txt_path = os.path.join(BASE_PATH,'Clean_MIDIs-path_to_cal500_path.txt')
+path_file = open(txt_path, 'rb')
+filereader = csv.reader(path_file, delimiter = '\t')
+may_30_reader = csv.reader(open('../../CSV_Analysis/5-30-14_Alignment_Results.csv'), delimiter = ',')
+may_30_reader.next()
+for row1 in filereader:
+  row2 = may_30_reader.next()[0]
+  try:
+    run_check(row1)
+    if row1[0] not in row2:
+      print "WARNING NOT IN MAY 30"
+      print row2
+  except:
+    print "ERROR ON "+row1[0]
 path_file.close()
--- experiments/midi_alignment_using_txt.py
+++ experiments/midi_alignment_using_txt.py
@@ -75,7 +75,7 @@ def to_chroma_npy(filename):
 
 def make_midi_cqt(midi_filename, piano, chroma, midi_info = None):
   if midi_info is None:
-    midi_info = pretty_midi.PrettyMIDI(midi.read_midifile(midi_filename))
+    midi_info = pretty_midi.PrettyMIDI(midi_filename)
   if piano:
     print "Generating CQT with piano roll"
     midi_gram = align_midi.midi_to_piano_cqt(midi_info)
@@ -116,7 +116,7 @@ def align_one_file(mp3_filename, midi_filename, output_midi_filename, output_dia
     '''
     # Load in the corresponding midi file in the midi directory, and return if there is a problem loading it
     try:
-        m = pretty_midi.PrettyMIDI(midi.read_midifile(midi_filename))
+        m = pretty_midi.PrettyMIDI(midi_filename)
     except:
         print "Error loading {}".format(midi_filename)
         return
@@ -183,11 +183,11 @@ def align_one_file(mp3_filename, midi_filename, output_midi_filename, output_dia
       print "Creating CQT for {}".format(os.path.split(midi_filename)[1])
       # Generate synthetic MIDI CQT
       midi_gram = make_midi_cqt(midi_filename, piano,chroma, m)
-
-    # midi_gram = align_midi.accentuate_onsets(midi_gram)
-    midi_gram = align_midi.piano_roll_fuzz(midi_gram)
-    # midi_gram = align_midi.clean_audio_gram(midi_gram, threshold = np.percentile(midi_gram,40))
-    midi_gram = librosa.util.normalize(midi_gram, axis = 0)
+    if piano:
+      # midi_gram = align_midi.accentuate_onsets(midi_gram)
+      midi_gram = align_midi.piano_roll_fuzz(midi_gram)
+      # midi_gram = align_midi.clean_audio_gram(midi_gram, threshold = np.percentile(midi_gram,40))
+      midi_gram = librosa.util.normalize(midi_gram, axis = 0)
 
     # Compute beats
     midi_beats, bpm = align_midi.midi_beat_track(m)
