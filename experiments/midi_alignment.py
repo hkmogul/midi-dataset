9739c72735abc30d0c4ea910e6f42a3a7a8fc1dd
HKMogul <hilary.mogul@gmail.com>
2014-07-24 16:46:42 -0400

    fix for match with new pretty midi syntax

--- experiments/midi_alignment.py
+++ experiments/midi_alignment.py
@@ -106,7 +106,7 @@ def align_one_file(mp3_filename, midi_filename, output_midi_filename, output_dia
 
     # Cache audio CQT and onset strength
 
-    
+
 
     audio, fs = librosa.load(mp3_filename)
     if use_mp3_data:
