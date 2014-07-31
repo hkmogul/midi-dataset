a42e8382c9c8f3a3db4924c95c9f59bce2725e48
HKMogul <hilary.mogul@gmail.com>
2014-07-31 13:59:52 -0400

    can now collect all 19 datapoints for classifier via a single function

--- alignment_analysis.py
+++ alignment_analysis.py
@@ -4,6 +4,10 @@ import sys
 import scipy.io
 import pretty_midi
 import scipy.stats
+import librosa
+import pretty_midi
+import scipy.signal
+
 ''' Module of functions for running comparisons of different paths '''
 
 def compare_paths(p1,q1,p2,q2, percent_in = 0):
@@ -153,3 +157,122 @@ def truncate_greater_vec(vec1, vec2):
 def util_print(data_y, data_names):
   for i in xrange(data_y.shape[0]):
     print '{0}, {1}'.format(data_names[i], data_y[i])
+
+
+
+def get_X(mat_file_path, mp3_path, old_midi_path, aligned_midi_path):
+  ''' gets the current version of the vector used for the classifier '''
+  vec = np.zeros((0,))
+
+  # load all relevant files
+  mat_file = scipy.io.loadmat(mat_file_path)
+  comp_mp3 = librosa.load(mp3_path, mono = 0)
+  aligned_midi = pretty_midi.PrettyMIDI(aligned_midi_path)
+  old_midi = pretty_midi.PrettyMIDI(old_midi_path)
+  # 1. weighted score
+  score = mat_file['score']
+
+  vec = np.append(vec, score)
+  # 2. magnitude of sim_mat
+  sim_mat = mat_file['similarity_matrix']
+  norm_mat = np.sum(sim_mat)/(sim_mat.shape[0]*sim_mat.shape[1])
+
+  vec = np.append(vec, norm_mat)
+  # 3-5 - r value, stderr, and intercept of offset path
+  offsets, note_ons = get_offsets(aligned_midi, old_midi)
+  slope, intercept, r, p_err, stderr = get_regression_stats(aligned_midi, old_midi, offsets, note_ons)
+
+  vec = np.append(vec, r)
+
+  vec = np.append(vec, stderr)
+
+  vec = np.append(vec, intercept)
+
+  # 6 variance of all offsets
+
+  vec = np.append(vec, np.var(offsets))
+
+
+  # 7-9 r, stderr, intercept of first 20% of offsets
+  slope, intercept, r, p_err, stderr = get_regression_stats(aligned_midi, old_midi, offsets[0:offsets.shape[0]*.2], note_ons[0:offsets.shape[0]*.2])
+
+  vec = np.append(vec, r)
+
+
+  vec = np.append(vec, stderr)
+
+
+  vec = np.append(vec, intercept)
+
+  # 10-11 variance of cost path and filtered cost path
+  cost_path = get_cost_path(mat_file['p'],mat_file['q'],sim_mat)
+
+  vec = np.append(vec, np.var(cost_path))
+
+  cost_path_filtered = np.copy(cost_path)
+  size = cost_path_filtered.shape[0]/2
+  if size % 2 == 0:
+    size +=1
+
+  cost_path_filtered = scipy.signal.medfilt(cost_path, kernel_size = size)
+  start = int(cost_path_filtered.shape[0]*.05)
+  end = int(cost_path_filtered.shape[0]*.95)
+  cost_path_filtered = cost_path_filtered[start:end]
+
+  vec = np.append(vec, np.var(cost_path_filtered))
+
+  # 12/13 np.var(residuals) from doing parabolic fit between filtered and original
+  p, parab,residuals = parabola_fit(cost_path_filtered)
+  vec = np.append(vec, np.var(residuals))
+
+
+  vec = np.append(vec, np.var(np.subtract(cost_path[start:end], parab)))
+
+  # 14 ratio of end time to max of first 10 percent of offset
+  first10 = offsets[0:int(.1*offsets.shape[0])]
+  vec = np.append(vec, float(np.amax(first10))/old_midi.get_end_time())
+
+  # cosine distance of loaded comparison files
+  beat_path = '../data/beat_info'
+  comp_audio, fs = librosa.load(mp3_path, mono = False)
+  # print "shape of comp_audio {}".format(comp_audio.shape)
+  midi_audio = comp_audio[0,:]
+  mp3_audio = comp_audio[1,:]
+  cosine = np.dot(midi_audio, mp3_audio)/(np.linalg.norm(midi_audio)*np.linalg.norm(mp3_audio))
+  beat_mat_path = os.path.join(beat_path, os.path.splitext(os.path.basename(mp3_path))[0]+'.mat')
+  # print "beat mat path {}".format(beat_mat_path)
+  # beat track differences
+  # cache for other runs
+  if not os.path.exists(beat_mat_path):
+    m_tempo, m_beats = librosa.beat.beat_track(midi_audio, fs)
+    a_tempo, a_beats = librosa.beat.beat_track(mp3_audio, fs)
+    scipy.io.savemat(beat_mat_path, {'m_beats': m_beats,
+                                                             'm_tempo': m_tempo,
+                                                             'a_beats': a_beats,
+                                                             'a_tempo': a_tempo})
+  else:
+    beat_mat = scipy.io.loadmat(beat_mat_path)
+    m_tempo = beat_mat['m_tempo'][0,0]
+    m_beats = beat_mat['m_beats'][0,:]
+    a_tempo = beat_mat['a_tempo'][0,0]
+    a_beats = beat_mat['a_beats'][0,:]
+  m_beatsP, a_beatsP, amt_pad = pad_lesser_vec(m_beats, a_beats)
+  m_beats, a_beats, amt_trunc = truncate_greater_vec(m_beats, a_beats)
+  beat_diff = librosa.frames_to_time(np.absolute(np.subtract(m_beats, a_beats)))
+  temp_mod = abs((m_tempo%2)-(a_tempo%2))
+  beat_cosine = np.dot(m_beats, a_beats)/(np.linalg.norm(m_beats)*np.linalg.norm(a_beats))
+
+  # aggregate features and targets
+  vec = np.append(vec, cosine)
+
+  vec = np.append(vec, beat_cosine)
+
+
+  vec = np.append(vec, temp_mod)
+
+  vec = np.append(vec, abs(m_tempo-a_tempo))
+
+  vec = np.append(vec, amt_pad)
+
+  print vec.shape
+  return vec
